a1dd6b402c6a49120c549b6303d6b3119926aa04
knokko <knokogator@hotmail.com>
2019-12-13 11:36:07 +0100

    Add boxed_clone function

--- src/levels/content/component/mod.rs
+++ src/levels/content/component/mod.rs
@@ -2,6 +2,8 @@ use web_sys::WebGlRenderingContext;
 
 pub trait LevelComponent {
 
+    fn boxed_clone(&self) -> Box<dyn LevelComponent>;
+
     fn render(&self, gl: &WebGlRenderingContext);
 
     fn get_render_bounds(&self);
